fe3398d78b59e2064273168ff65ad0d799a81517
Alexander Zenov <alexander.zenov@gmail.com>
2020-05-04 17:12:40 +0300

    Add routes

---
+++ src/users/routes.rs
@@ -0,0 +1,46 @@
+use crate::users::{User, Users};
+use crate::error_handler::CustomError;
+
+use actix_web::{delete, get, post, put, web, HttpResponse};
+use serde_json::json;
+
+#[get("/users")]
+async fn find_all() -> Result<HttpResponse, CustomError> {
+    let users = Users::find_all()?;
+    Ok(HttpResponse::Ok().json(users))
+}
+
+#[get("/users/{id}")]
+async fn find(id: web::Path<i32>) -> Result<HttpResponse, CustomError> {
+    let user = Users::find(id.into_inner())?;
+    Ok(HttpResponse::Ok().json(user))
+}
+
+#[post("/users")]
+async fn create(user: web::Json<User>) -> Result<HttpResponse, CustomError> {
+    let user = Users::create(user.into_inner())?;
+    Ok(HttpResponse::Ok().json(user))
+}
+
+#[put("/users/{id}")]
+async fn update(
+    id: web::Path<i32>,
+    user: web::Json<User>,
+) -> Result<HttpResponse, CustomError> {
+    let user = Users::update(id.into_inner(), user.into_inner())?;
+    Ok(HttpResponse::Ok().json(user))
+}
+
+#[delete("/users/{id}")]
+async fn delete(id: web::Path<i32>) -> Result<HttpResponse, CustomError> {
+    let deleted_user = Users::delete(id.into_inner())?;
+    Ok(HttpResponse::Ok().json(json!({ "deleted": deleted_user })))
+}
+
+pub fn init_routes(comfig: &mut web::ServiceConfig) {
+    comfig.service(find_all);
+    comfig.service(find);
+    comfig.service(create);
+    comfig.service(update);
+    comfig.service(delete);
+}
